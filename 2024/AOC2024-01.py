88712f703d495372a535df7818e878af181c2de9
franz <fneumeister@in-technology.eu>
2024-12-02 12:01:50 +0100

    D1P2 solution

--- 2024/AOC2024-01.py
+++ 2024/AOC2024-01.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from collections import defaultdict, Counter
 
 
 def main():
@@ -7,12 +8,14 @@ def main():
     
     LL = []
     RR = []
+    RC = Counter()
 
     for line in lines:
         L,R = line.split()
         L,R = int(L), int(R)
         LL.append(L)
         RR.append(R)
+        RC[R] += 1
 
     LL = sorted(LL)
     RR = sorted(RR)
@@ -20,8 +23,13 @@ def main():
     ans = 0
     for l,r in zip(LL,RR):
         ans += abs(l - r)
+    print(f"Solution part 1: {ans}")
 
-    print(ans)
+    ans2 = 0
+    for l in LL:
+        ans2 += l * RC.get(l, 0)
+
+    print(f"Solution part 2: {ans2}")
 
 if __name__ == "__main__":
     main()

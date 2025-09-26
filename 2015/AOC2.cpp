d69dc0d44823b7933d0ce74aa5e90777e787129f
Franz Neumeister <neumeister.franz@gmx.de>
2025-09-26 22:18:58 +0200

    Adding reading from with delimiter

--- 2015/AOC2.cpp
+++ 2015/AOC2.cpp
@@ -1,12 +1,14 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-
+#include <sstream>
+ 
 int main ( ) {
-    std::ifstream inputfile("AOC2_input");
-    std::string input;
-    std::getline(inputfile, input);
-    inputfile.close();
+    std::ifstream input2("AOC2_input");
+    for (std::string line; std::getline(input2, line, 'x');)
+        std::cout << line << '\n';
 
-    return 0;
+
+
+	return 0;
 }

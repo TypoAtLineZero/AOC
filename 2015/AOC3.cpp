b94e79c3ea533a5a069eb10d0d43d70a2a87eab8
Franz Neumeister <neumeister.franz@gmx.de>
2025-10-16 12:50:02 +0200

    Starting with 2d array approach

--- 2015/AOC3.cpp
+++ 2015/AOC3.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
-#include <vector>
-#include <algorithm>
-using namespace std;
 
 int main() {
-    string filename="AOC2_input";
+	std::string filename="AOC2_input";
 
-    ifstream file(filename);
+	std::ifstream file(filename);
     if (!file.is_open()) {
-        cerr << "Error opening the file." << endl;
+			std::cerr << "Error opening the file." << std::endl;
         return 1;
     }
 
+    int arr[4][2] = {
+        { 10, 11 },
+        { 20, 21 },
+        { 30, 31 },
+        { 40, 41 }
+    };
+
+    int i, j;
+
+	std::cout << "Printing a 2D Array:\n";
+    for (i = 0; i < 4; i++) {
+        for (j = 0; j < 2; j++) {
+				std::cout << "\t" << arr[i][j];
+        }
+		std::cout << std::endl;
+    }
+    
     return 0;
 }
-

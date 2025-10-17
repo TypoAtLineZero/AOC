98eabbe9f2b5781e681b3410fa79fe763c471cba
Franz Neumeister <neumeister.franz@gmx.de>
2025-10-17 21:30:21 +0200

    Adding loop for array

--- 2015/AOC3.cpp
+++ 2015/AOC3.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 
 int main() {
-	std::string filename="AOC2_input";
+	std::string filename="AOC3_input";
 
 	std::ifstream file(filename);
     if (!file.is_open()) {
@@ -10,22 +10,22 @@ int main() {
         return 1;
     }
 
-    int arr[4][2] = {
-        { 10, 11 },
-        { 20, 21 },
-        { 30, 31 },
-        { 40, 41 }
-    };
+    int arr[1000][1000] = {0};
+    std::string line;
 
-    int i, j;
+	while (getline(file, line)) {
+			std::cout << line << std::endl;
+	}
 
-	std::cout << "Printing a 2D Array:\n";
-    for (i = 0; i < 4; i++) {
-        for (j = 0; j < 2; j++) {
-				std::cout << "\t" << arr[i][j];
-        }
-		std::cout << std::endl;
-    }
+    //int i, j;
+
+	//std::cout << "Printing a 2D Array:\n";
+    //for (i = 0; i < 4; i++) {
+    //    for (j = 0; j < 2; j++) {
+	//			std::cout << "\t" << arr[i][j];
+    //    }
+	//	std::cout << std::endl;
+    //}
     
     return 0;
 }

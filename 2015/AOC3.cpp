cb51589fa312d875e0ce0b6133a733cb4f9eaa34
Franz Neumeister <neumeister.franz@gmx.de>
2025-11-07 09:07:07 +0100

    Adding solution to D3P2

--- 2015/AOC3.cpp
+++ 2015/AOC3.cpp
@@ -16,6 +16,9 @@ int main() {
     int arr[1000][1000] = {0};
 	int i = 500;
     int j = 500;
+    int i_rob = 500;
+    int j_rob = 500;
+    int turn = 0;
     std::cout << i  << j << std::endl;
 	arr[i][j] = 1;
     
@@ -26,20 +29,42 @@ int main() {
         // > - 62
         // ^ - 94
         // V - 118
-        if (ii == 60) {
-				--i;
-		} else if (ii == 62) {
-				++i;
-		} else if (ii == 94) {
-				--j;
-		} else if (ii == 118) {
-				++j;
-		} else {
-				std::cout << "Reached end" << std::endl;
+
+        // Santas turn
+        if ( turn % 2 == 0) {
+            std:: cout << "Santas turn" << std::endl;
+            if (ii == 60) {
+                --i;
+            } else if (ii == 62) {
+                ++i;
+            } else if (ii == 94) {
+                --j;
+            } else if (ii == 118) {
+                ++j;
+            } else {
+                std::cout << "Reached end" << std::endl;
                 break;
-		}
+            }
+        // Robos turn
+        } else {
+            std:: cout << "Robos turn" << std::endl;
+            if (ii == 60) {
+                --i_rob;
+            } else if (ii == 62) {
+                ++i_rob;
+            } else if (ii == 94) {
+                --j_rob;
+            } else if (ii == 118) {
+                ++j_rob;
+            } else {
+                std::cout << "Reached end" << std::endl;
+                break;
+            }
+        }
 
 		arr[i][j] = 1;
+        arr[i_rob][j_rob] = 1;
+        ++turn;
 	}
 
 	int counter = 0;

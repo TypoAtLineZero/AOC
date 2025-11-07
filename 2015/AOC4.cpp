ccfb083f9a7550b474e4c86040a6ed29a61b7d29
Franz Neumeister <neumeister.franz@gmx.de>
2025-11-07 22:40:04 +0100

    Adding md5 lib

--- 2015/AOC4.cpp
+++ 2015/AOC4.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
-int main() {
-	std::string filename="yzbqklnj";
+// Taken from
+// http://www.zedwood.com/article/cpp-md5-function
+#include "md5.h"
 
+int main() {
+	std::string hash_pre = "yzbqklnj";
+    std::string hash_suff = "000000";
+    std::string hash = hash_pre + hash_suff;
+
+    std::cout << "hash: " << md5(hash) << std::endl;
 	return 0;
 }

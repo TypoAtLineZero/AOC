4d773c435fd958a7fd6e6944474311cfe6f06149
Franz Neumeister <neumeister.franz@gmx.de>
2025-11-10 14:49:42 +0100

    Adding suiting parsing mechanism

--- 2015/AOC5.cpp
+++ 2015/AOC5.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <fstream>
 #include <sstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
 int main() {
+    string filename="AOC5_input";
 
-	return 0;
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cerr << "Error opening the file." << endl;
+        return 1;
+    }
+
+    long total_paper = 0; // Wrapping paper
+    long total_ribbon = 0; // Ribbon
+    string line;
+
+    while (getline(file, line)) {
+        vector<int> dimensions;
+        stringstream ss(line);
+        string part;
+    }
+    
+    return 0;
 }
+
+

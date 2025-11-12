21b39d0f5c6b5a61965807c64cc91f61dee9979f
Franz Neumeister <neumeister.franz@gmx.de>
2025-11-12 14:13:47 +0100

    Adding first steps for parsing

--- 2015/AOC6.cpp
+++ 2015/AOC6.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-using namespace std;
 
 int main() {
     std::string filename="AOC6_input";
-
-    ifstream file(filename);
+    std::ifstream file(filename);
+   
     if (!file.is_open()) {
         std::cerr << "Error opening the file." << std::endl;
         return 1;
@@ -15,12 +14,16 @@ int main() {
     std::string line;
     std::string part;
     
+    bool grid[1000][1000] = {false};
+    
     while (getline(file, line)) {
-        stringstream ss(line);
-
-        // Split each dimension by 'x'
-        while (getline(ss, part, 'x')) {
+        std::cout << "line1: " << line << std::endl;
+        if (line.find("on") != std::string::npos) {
+            std::cout << "on" << std::endl;
         }
+        // Split each dimension by 'x'
+        //while (getline(ss, part, 'x')) {
+        //}
     }
 
     file.close();

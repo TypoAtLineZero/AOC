210a8324f9bef999b67320768b53d96c7d16bb2a
Franz Neumeister <neumeister.franz@gmx.de>
2025-12-07 22:51:05 +0100

    Adding counter for regex

--- 2015/AOC08.cpp
+++ 2015/AOC08.cpp
@@ -15,7 +15,6 @@ int main(void) {
 
     std::string line;
     std::regex hexNum { R"([\\][x][0-9][0-9])" };
-    std::regex hexNum2("([\\][x][0-9][0-9])");
     std::regex doubleBackslash { R"([\\][\\])" };
     std::regex doubleQuote { R"([\\][\"])" };
     std::smatch sm;
@@ -32,10 +31,13 @@ int main(void) {
         bool foundHexNum = false;
         bool foundDoubleBackslash = false;
         bool foundDoubleQuote = false;
-
+        
         foundHexNum = std::regex_search(line, hexNum);
         std::regex_match(line, sm, hexNum);
-        std::regex_match(line, sm2, hexNum2);
+
+        std::ptrdiff_t const matchCountHexNum(std::distance(std::sregex_iterator(line.begin(), line.end(), hexNum), std::sregex_iterator()));
+        std::ptrdiff_t const matchCountBackslash(std::distance(std::sregex_iterator(line.begin(), line.end(), doubleBackslash), std::sregex_iterator()));
+        std::ptrdiff_t const matchCountDoubleQuote(std::distance(std::sregex_iterator(line.begin(), line.end(), doubleQuote), std::sregex_iterator()));
        
         foundDoubleBackslash = std::regex_search(line, doubleBackslash);
         foundDoubleQuote = std::regex_search(line, doubleQuote);
@@ -45,8 +47,7 @@ int main(void) {
 
         if (foundHexNum) {
             std::cout << "found Hexnum" << std::endl;
-            std::cout << "hexnum counter: " << sm.size() << std::endl;
-            std::cout << "hexnum counter 2: " << sm2.position() << std::endl;
+            std::cout << match_count << std::endl;
         }
         
         if (foundDoubleBackslash) {

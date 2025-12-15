2225d8d76aae728f4984e5aab16b0b57f5af3489
Franz Neumeister <neumeister.franz@gmx.de>
2025-12-15 21:16:27 +0100

    Adding AOC10.cpp

---
+++ 2015/AOC10.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <regex>
+#include <vector>
+#include <cstdint>
+
+int main(void) {
+    uint64_t input = 3113322113;
+
+    return 0;
+}

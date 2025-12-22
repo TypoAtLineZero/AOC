b45906cea6caeb7c51a95d7bf4a0f0f3cbf9abd3
Franz Neumeister <neumeister.franz@gmx.de>
2025-12-22 20:55:26 +0100

    Get groundframe working

--- 2015/AOC09.cpp
+++ 2015/AOC09.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
+#include <sstream>
 #include <fstream>
 #include <string>
 #include <regex>
 #include <vector>
 
 int main(void) {
-    std::string filename="AOC09_input";
-
-    std::ifstream file(filename);
+    //std::string filename="AOC09_input";
+    std::ifstream file("AOC09_input");
+    std::string line;
+    
     if (!file.is_open()) {
         std::cerr << "Error opening the file." << std::endl;
-        return 1;
+        return 1;   std::string line;
     }
 
+    std::getline(std::cin, line);
+    std::istringstream linestream{line};       
+
+    while(std::getline(file, line)) {
+        std::istringstream iss(line);
+        std::cout << line << std::endl;
+    }
+    
     return 0;
 }
